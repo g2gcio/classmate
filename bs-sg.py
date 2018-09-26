27a215d41e80e08391de6d29d31e719b4e9367e4
g2gcio <g2gcio@gmail.com>
2018-09-26 23:02:02 +0800

    Update bs-sg.py

--- bs-sg.py
+++ bs-sg.py
@@ -4,6 +4,11 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 from bs4 import BeautifulSoup
 
+import dataset
+db = dataset.connect('sqlite:///sgtalk.db')
+
+sgtable = db['authors']
+
 page_list = ["https://sgtalk.org/mybb/Forum-Market-Talk",
             "https://sgtalk.org/mybb/Forum-Market-Talk?page=2",
             "https://sgtalk.org/mybb/Forum-Market-Talk?page=3",
@@ -18,10 +23,25 @@ page_list = ["https://sgtalk.org/mybb/Forum-Market-Talk",
             "https://sgtalk.org/mybb/Forum-Market-Talk?page=12",
             "https://sgtalk.org/mybb/Forum-Market-Talk?page=13",
             "https://sgtalk.org/mybb/Forum-Market-Talk?page=14",
-            "https://sgtalk.org/mybb/Forum-Market-Talk?page=15"
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=15",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=16",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=17",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=18",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=19",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=20",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=21",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=22",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=23",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=24",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=25",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=26",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=27",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=28",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=29",
+            "https://sgtalk.org/mybb/Forum-Market-Talk?page=30"
 ]
 
-#page_list = ["https://sgtalk.org/mybb/Forum-Market-Talk"]
+page_list = ["https://sgtalk.org/mybb/Forum-Market-Talk"]
 
 for list in page_list:
     page = requests.get(list, verify=False)
@@ -29,8 +49,9 @@ for list in page_list:
     table = soup.find_all('tr', class_='inline_row')
     for t in table:
         subject = t.find('span', class_="subject_new")
-        subject_title = subject.find('a')
+        subject_title = subject.find('a').get("href").replace('Thread-','')
         author = t.find('div', class_="author smalltext")
         author_name = author.find('a')
-        print(subject_title.get('href'),",",author_name.get('href').replace('https://sgtalk.org/mybb/',''))
-
+        author_name1=author_name.get('href').replace('https://sgtalk.org/mybb/User-','')
+        print(subject_title,",",author_name1)
+#        sgtable.insert(dict(name=author_name1, article=subject_title))
